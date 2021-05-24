865a6fc05be425534b5ce1efe2dab8f7fa880f25
2021-01-06 <77026510+2021-01-06@users.noreply.github.com>
2021-05-24 13:55:16 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-  printf("<h1>Hello Hongik!</h1>")
-  printf("<h2>Hello Hongik!</h2>")
-  printf("<h3>Hello Hongik!</h3>")  
-  return 0
+  printf("<h1>Hello Hongik!</h1>");
+  printf("<h2>Hello Hongik!</h2>");
+  printf("<h3>Hello Hongik!</h3>"); 
+  return 0;
 }
